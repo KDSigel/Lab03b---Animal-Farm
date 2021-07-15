7208d9e6d2edb450cb4a4a4b9d63b827dc8a8f93
Karl Sigel <heykarl@gmail.com>
2021-07-15 11:05:18 -0700

    added images and sounds

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 // import functions and grab DOM elements
+const animalNameDisplay = document.getElementById('animal-name');
 
+const horseEl = document.getElementById('horse-image');
+const dogEl = document.getElementById('dog-image');
+const catEl = document.getElementById('cat-image');
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
 // initialize state
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+horseEl.addEventListener('click', () => {
+    horseSound.play();
+    animalNameDisplay.textContent = 'This is a horse';
+});
+
+dogEl.addEventListener('click', () => {
+    dogSound.play();
+    animalNameDisplay.textContent = 'This is a dog';
+});
+
+catEl.addEventListener('click', () => {
+    catSound.play();
+    animalNameDisplay.textContent = 'This is a cat. If it were as big as a dog or horse, it would kill you.';
+});
+
+// trying key stuff 
+
+document.addEventListener("keydown", function(event) {
+  if (event.code === "KeyD") {
+      dogSound.play();
+  animalNameDisplay.textContent = 'D is for Dog';
+}});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyH") {
+        horseSound.play();
+    animalNameDisplay.textContent = 'H is for Horse';
+}});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyC") {
+        catSound.play();
+    animalNameDisplay.textContent = 'C is for Cat';
+}});
